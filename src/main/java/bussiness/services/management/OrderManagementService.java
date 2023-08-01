1d8faacbd358c9a4de4438c720b0bd3c34bc92dd
kamarbaraka <kamar254baraka@gmail.com>
2023-08-01 22:35:07 +0300

    initial commit
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/bussiness/services/management/OrderManagementService.java
+++ src/main/java/bussiness/services/management/OrderManagementService.java
@@ -1,4 +1,37 @@
 package bussiness.services.management;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import persistence.ItemOrder;
+
 public class OrderManagementService {
+
+    public static void main(String[] args) {
+
+        try (EntityManagerFactory factory = Persistence.createEntityManagerFactory("inventory_management")) {
+
+            try (EntityManager manager = factory.createEntityManager()) {
+
+                EntityTransaction transaction = manager.getTransaction();
+
+                transaction.begin();
+
+                ItemOrder itemOrder = new ItemOrder();
+
+                /*persist*/
+                manager.persist(itemOrder);
+
+                /*retrieve*/
+                ItemOrder itemOrder = manager.createQuery("select item from ItemOrder item ", ItemOrder.class).getSingleResult();
+
+
+                transaction.commit();
+
+            }
+
+        }
+    }
+
 }
