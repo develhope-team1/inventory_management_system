601b6f77e9f07a90a0e0655c4d611fe9174b265b
kamarbaraka <kamar254baraka@gmail.com>
2023-08-03 00:22:07 +0300

    implemented the InventoryTrackingService
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/bussiness/services/tracking/InventoryTrackingService.java
+++ src/main/java/bussiness/services/tracking/InventoryTrackingService.java
@@ -1,4 +1,82 @@
 package bussiness.services.tracking;
 
-public class InventoryTrackingService {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import persistence.ItemInventory;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ *  a class to simulate an inventory tracker.
+ *  @author kamar baraka.*/
+
+public class InventoryTrackingService implements Runnable{
+
+    private final String PERSISTENCE_UNIT = "inventory_management";
+
+    @Override
+    public void run() {
+
+        /*construct an entity manager factory*/
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+
+        /*construct an entity manager */
+        try {
+
+            EntityManager manager = factory.createEntityManager();
+
+            /*get the transaction*/
+            EntityTransaction transaction = manager.getTransaction();
+
+            /*check and begin transaction*/
+            if (!transaction.isActive())
+                transaction.begin();
+
+            /*continuously check and notify when inventory count is low.*/
+            try
+            {
+                while (true) {
+
+                    /*create query*/
+                    List<ItemInventory> itemInventories = manager.createQuery(
+                            "select itemInventory from ItemInventory itemInventory where count<5",
+                            ItemInventory.class
+                    ).getResultList();
+
+                    /*report if less*/
+                    Toolkit.getDefaultToolkit().beep();
+                    itemInventories.forEach(itemInventory -> System.err.printf("%s ", itemInventory.getInventoryName()));
+
+                    /*wait for 2 mins before the next check*/
+                    Thread.sleep(12000);
+
+                }
+            }
+            catch (InterruptedException exception){
+
+                /*ignore*/
+            }
+        }
+        finally {
+
+            /*check and close the factory*/
+            if (factory.isOpen())
+                factory.close();
+        }
+
+    }
+
+    /**
+     * setting up the service*/
+    public static Thread setup(){
+        /*construct the tracking service*/
+        InventoryTrackingService service = new InventoryTrackingService();
+
+        /*construct a thread for the service*/
+        return new Thread(service);
+    }
+
 }
