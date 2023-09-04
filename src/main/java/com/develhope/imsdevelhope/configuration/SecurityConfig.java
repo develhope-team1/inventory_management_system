92b098a1e10fcda81660011f072a8be41b5ab3ca
kylerobins <darkspidersteewy@gmail.com>
2023-09-04 14:40:03 +0300

    service updates

---
+++ src/main/java/com/develhope/imsdevelhope/configuration/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.develhope.imsdevelhope.configuration;
+
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import com.develhope.imsdevelhope.app.business.services.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/users")).authenticated()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .usernameParameter("email")
+                        .defaultSuccessUrl("/users")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+        http.authenticationProvider(authenticationProvider());
+
+        return http.build();
+    }
+}
